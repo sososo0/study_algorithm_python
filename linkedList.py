0dfdea65262d989b5f4d92df0b5967cb4f9f8eaf
sososo0 <sh0000@pusan.ac.kr>
2023-12-13 15:54:54 +0900

    feat: Add comment in linkedList

--- linkedList.py
+++ linkedList.py
@@ -2,7 +2,9 @@ edges = [[0, 1], [0, 2], [1, 2], [2, 3], [2, 5], [3, 4], [3, 5]]
 graph = [[] for _ in range(6)]
 
 for a, b in edges:
-    graph[a].append(b)
-    graph[b].append(a)
+    graph[a].append(b) 
+    graph[b].append(a) 
+    # 단 방향 graph 인 경우는 방향대로 1번만 append 한다.
+    # 위의 경우는 양방향인 경우 
     
 print(graph)
