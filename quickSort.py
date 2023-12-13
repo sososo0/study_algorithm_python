1f428a6e21999d0dae3429d7f946a076c109d7b5
sososo0 <sh0000@pusan.ac.kr>
2023-12-13 17:48:36 +0900

    feat: Add quickSort code

---
+++ quickSort.py
@@ -0,0 +1,25 @@
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+def quickSort(array, start, end):
+    if start >= end:
+        return 
+    
+    pivot = start 
+    left = start + 1 
+    right = end 
+    
+    while left <= right:
+        while (left <= end and array[left] <= array[pivot]):
+            left += 1
+        while (right > start and array[right] >= array[pivot]):
+            right -= 1
+        if (left > right):
+            array[right], array[pivot] = array[pivot], array[right]
+        else:
+            array[left], array[right] = array[right], array[left]
+
+    quickSort(array, start, right-1)
+    quickSort(array, right+1, end)
+    
+quickSort(array, 0, len(array) - 1)
+print(array)
